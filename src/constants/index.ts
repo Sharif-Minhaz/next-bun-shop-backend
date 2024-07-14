507636b544497dab1ac008825b2a400889602880
Minhaz <sharifmdminhazurrahman@gmail.com>
2024-07-14 17:24:51 +0600

    fix: domain issue

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
-export const DOMAIN = "next-bun-shop-backend.onrender.com";
+export const DOMAIN = "next-bun-shop-frontend.vercel.app";
+// export const DOMAIN = "next-bun-shop-backend.onrender.com";
 // export const DOMAIN = "localhost";
